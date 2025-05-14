8c7177e14604661d3c8e2eb71abfa031459272ea
Garth N. Wells <gnw20@cam.ac.uk>
2025-05-14 04:45:00 +0000

    Add MPI dep

--- spack/packages/bench-dolfinx/package.py
+++ spack/packages/bench-dolfinx/package.py
@@ -14,7 +14,7 @@ class BenchDolfinx(CMakePackage, CudaPackage, ROCmPackage):
     depends_on("py-fenics-ffcx@main", type="build")
     depends_on("py-fenics-ufl@main", type="build")
     # depends_on("py-setuptools", type="build")
-    # depends_on("mpi")
+    depends_on("mpi")
     depends_on("hip", when="+rocm")
     depends_on("cuda", when="+cuda")
 
